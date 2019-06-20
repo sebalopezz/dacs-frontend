6614c4ab113b3265b97b53867f2484335293a3be
sebalopezz <jorgesebastianlopez@gmail.com>
2019-06-20 20:03:34 -0300

    funciones que tendria el medico

--- componentes/medico.js
+++ componentes/medico.js
@@ -4,8 +4,10 @@ const Medico = {
         <div>
             <h1> Bienvenido </h1>
             <h2> Esta es la pagina de medicos </h2>
-            <router-link to="/create" class="btn btn-primary"> Nueva historia clinica </router-link><br><br>
-            <router-link to="/" class="btn btn-primary"> Registrar visita </router-link>
+            <router-link to="/" class="btn btn-success"> Registrar visita sin turno </router-link>
+            <router-link to="/create" class="btn btn-success"> Nueva historia clinica </router-link><br><br>
+            <router-link to="/medico/historiasclinicas" class="btn btn-primary"> Ver historias clínicas</router-link><br><br>
+            <router-link to="/create" class="btn btn-primary"> Ver próximos turnos </router-link><br><br>
             <h2> Próximos turnos </h2>
 
         </div>
