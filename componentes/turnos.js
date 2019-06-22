f256f665fc907d657ab447b78ab5624ab0a03cf4
Gerardo Enrique <geraenrique97>
2019-06-22 19:37:48 -0300

    merge con seba

--- componentes/turnos.js
+++ componentes/turnos.js
@@ -1,36 +1,37 @@
 const Turnos={
     template:`
-<h2 id="title-turnos">Turnos al dia: 22/06</h2>
-<table class="table" id="table-turnos">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Nombre</th>
-        <th scope="col">Horario</th>
-        <th scope="col">Atender</th>
-        <th scope="col">Ausente</th>
+<div>
+  <h2 id="title-turnos">Turnos al dia: 22/06</h2>
+  <table class="table text-center" id="table-turnos">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Horario</th>
+          <th scope="col">Atender</th>
+          <th scope="col">Ausente</th>
 
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Gerardo Enrique</td>
-        <td>17:00</td>
-        <td> <input type="checkbox"></td>
-        <td><button type="button" class="btn btn-outline-danger">Quitar</button></td>
-      </tr>
-    </tbody>
-</table>
-
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Gerardo Enrique</td>
+          <td>17:00</td>
+          <td> <input type="checkbox"></td>
+          <td><button type="button" class="btn btn-outline-danger">Quitar</button></td>
+        </tr>
+      </tbody>
+  </table>
+</div>
       `,
     data(){
       return{
-        turnos=Object
+        turnos:Object
       }
     },
     create:function(){
-
+      console.log('nada')
     },
     methods:{
       getTurnos:function(){
